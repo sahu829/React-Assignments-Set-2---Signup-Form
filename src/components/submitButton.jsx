51c7b5f4c4c9af1f6017d2332c5f696c009aeaf1
sahu829 <55076453+sahu829@users.noreply.github.com>
2020-12-24 20:59:14 +0530

    Change

--- src/components/submitButton.jsx
+++ src/components/submitButton.jsx
@@ -1,12 +1,15 @@
+import { Link } from "@material-ui/core";
 import React from "react";
 
 export default function SubmitButton() {
+  const [value, setValue] = React.useState("");
   const handleClick = () => {
-    "Hello krishna";
+    setValue("helo krishna");
   };
 
   return (
     <>
+      {value}
       <button data-testid="submit" onClick={handleClick}>
         Submit
       </button>
